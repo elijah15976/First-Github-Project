64d071456d4008d8d7c96816100b27aebe48f7c7
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2021-04-12 13:06:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import Random
+
 print("Hello, this is a test file")
+
+
+num = random.randint(1, 2)
+
+if num == 1:
+  print("Number 1")
+elif num == 2:
+  print("Number 2")
+else:
+  print("Something went wrong, please try again.")
