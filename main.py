e30d983cdcdd8fedb91d8611047f82205cd93f4b
elijah15976 <72220510+elijah15976@users.noreply.github.com>
2023-09-13 10:51:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print("Hello, this is a test file")
 num = random.randint(1, 2)
 
 if num == 1:
-  print("Number 1")
+  print("Number is not 2")
 elif num == 2:
-  print("Number 2")
+  print("Number is not 1")
 else:
   print("Something went wrong, please try again.")
